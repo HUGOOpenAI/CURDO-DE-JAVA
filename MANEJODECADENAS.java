7cd3526fada7607b8a5d3b4cd7ddc7acc74998b9
HUGOOpenAI <48729768+HUGOOpenAI@users.noreply.github.com>
2019-04-01 16:37:04 +0200

    Add files via upload

---
+++ MANEJODECADENAS.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manejo.de.cadenas;
+
+/**
+ *
+ * @author PC14-LAB02
+ */
+public class MANEJODECADENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre ="hugo";
+        String apellido = "velazco";
+        System.out.println("concatenacion:" + nombre + apellido);  
+        System.out.println("Nueva linea:\n"+nombre+" "+apellido);
+        System.out.println("tabulador:\t"+nombre+" "+apellido);
+        System.out.println("retroceso:\b\b\b"+nombre+" "+apellido);
+        System.out.println("retorno de carro:\r"+nombre+" "+apellido);
+        System.out.println("comilla simple:\'"+nombre+" "+apellido+"\'");
+        System.out.println("comilla doble:\""+nombre+" "+apellido+"\"");
+        
+        
+        
+        
+        
+        
+        
+                
+    }
+    
+}
